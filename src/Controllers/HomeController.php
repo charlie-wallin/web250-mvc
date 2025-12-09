bcef263b6bedb9302344c0c063ba17daf9779157
Charlie Wallin <charliekwallin@gmail.com>
2025-12-09 15:54:03 -0500

    Going to try AI rewriting this page so it displays the content as a table.

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -26,11 +26,4 @@ class HomeController
         include __DIR__ . '/../Views/about.php';
         return ob_get_clean();
     }
-
-    public function salamanderDetails(): string
-    {
-        ob_start();
-        include __DIR__ . '/../Views/salamander_details.php';
-        return ob_get_clean();
-    }
 }
